daa4092fcb5fceb0fcf617615326a8f79219d7b3
ILYAS MAIDI <83442258+ilyasmaidi@users.noreply.github.com>
2024-01-24 12:53:56 -0800

    footer get data from batabase

---
+++ admin/footer.php
@@ -0,0 +1,77 @@
+<?php 
+session_start();
+include ("../config/dbcon.php");
+include ("../middelwar/adminMiddleware.php");
+include("./includes/header.php"); 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+?>
+        <div class="container">
+
+          <div class="row">
+            <div class="col-md-12">
+                <?php
+                    if(isset($_GET['id']))
+                    {
+                        $id = $_GET['id'];
+        
+                        $topservices =GetByID("services",$id);
+
+                        if(mysqli_num_rows($topservices) > 0)
+                        {
+                                $data = mysqli_fetch_array($topservices);
+                        
+                                ?>
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4>Edit Services</h4>
+                                </div>
+                                <form action="code.php" method="POST" enctype="multipart/form-data">
+
+                                
+                                <div class="card-body">
+                                    <div class="row">
+
+                                        <div class="col-md-12">
+                                        <input type="hidden" name="service_id" value="<?=$data['id']?>">
+                                            <label for="">Title</label>
+                                            <input type="text" name="title" value="<?=$data['title']?>" class="form-control" placeholder="Enter .">
+
+                                        </div>
+                                        
+
+
+                                        <div class="col-md-12">
+                                            <label for="">Description</label> 
+                                            <textarea rows="3" name="text" placeholder="Enter Description" class="form-control"><?=$data['text']?></textarea>
+                                        </div>
+
+                                        
+
+                                        
+
+                                        <div class="com-md-12" style="margin: 8px 0;">
+                                            <button name="update_service_btn" type="submit" class="btn btn-primary">Update</button>
+                                        </div>
+
+
+                                    </div>
+                                    
+                                </div>
+
+                                </form>
+                            </div>
+                            <?php
+                        } else
+                        {
+                            echo "category not found";
+                        }
+                    } else{
+                        echo "Id Missing from url ";
+                    }
+                    ?>
+            </div>
+          </div>      
+            
+        </div>
+<?php include("./includes/footer.php") ?>
--- includes/footer.php
+++ includes/footer.php
@@ -1,169 +1,93 @@
+<?php
+$topsidebar = GetAll("sidebar");
+$data = mysqli_fetch_array($topsidebar);
+
+$topheader = GetAll("topheader");
+$newdata = mysqli_fetch_array($topheader);
+
+$topinstgram = GetAll("instagram_photos");
+?>
+
 <div class="footer-area pt-100 pb-70">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="single-footer-widget">
-                            <div class="logo">
-                                <h2>
-                                    <a href="index.php">AgriTech</a>
-                                </h2>
-                            </div>
-                            <p>The 2024 Global Forum for Food and Agriculture (GFFA) aims to address the pressing challenges of food security, sustainability, and equitable distribution of resources. agritech, an innovative Agricultural Extension Officer powered by Artificial Intelligence (AI), is uniquely positioned to contribute to each of the GFFA's key topics, including sustainable production, resilient food supply chains, food loss reduction, and support for vulnerable groups.</p>
-                            <ul class="social">
-                                <li>
-                                    <a href="https://www.facebook.com/profile.php?id=61555230390727&mibextid=ZbWKwL" class="facebook" target="_blank">
-                                        <i class='bx bxl-facebook'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" class="twitter" target="_blank">
-                                        <i class='bx bxl-twitter'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" class="pinterest" target="_blank">
-                                        <i class='bx bxl-pinterest-alt'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="https://www.linkedin.com/in/agritech-s-4774642a2?" class="linkedin" target="_blank">
-                                        <i class='bx bxl-linkedin'></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-3 col-sm-6">
+                <div class="single-footer-widget">
+                    <div class="logo">
+                        <h2><a href="index.php">AgriTech</a></h2>
                     </div>
+                    <p><?= $data['slug'] ?></p>
+                    <ul class="social">
+                        <?php foreach (['facebook', 'twitter', 'pinterest', 'linkedin'] as $socialMedia) : ?>
+                            <li>
+                                <a href="<?= $data[$socialMedia] ?>" class="<?= $socialMedia ?>" target="_blank">
+                                    <i class='bx bxl-<?= $socialMedia ?>'></i>
+                                </a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            </div>
 
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="single-footer-widget">
-                            <h3>Instagram</h3>
+            <div class="col-lg-3 col-sm-6">
+                <div class="single-footer-widget">
+                    <h3>Instagram</h3>
+                    <ul class="instagram-list">
+                        <?php while ($row = mysqli_fetch_assoc($topinstgram)) : ?>
+                            <li>
+                                <div class="box">
+                                    <img src="<?= $row['photo_url'] ?>" alt="image">
+                                    <i class="bx bxl-instagram"></i>
+                                    <a href="<?= $row['link_url'] ?>" target="_blank" class="link-btn"></a>
+                                </div>
+                            </li>
+                        <?php endwhile; ?>
+                    </ul>
+                </div>
+            </div>
 
-                            <ul class="instagram-list">
-                                <li>
-                                    <div class="box">
-                                        <img src="assets/img/instagram/instagram1.jpg" alt="image">
-                                        <i class="bx bxl-instagram"></i>
-                                        <a href="https://www.instagram.com/agrithec/" target="_blank" class="link-btn"></a>
-                                    </div>
-                                </li>
+            <!-- Other sections... -->
 
-                                <li>
-                                    <div class="box">
-                                        <img src="assets/img/instagram/instagram2.jpg" alt="image">
-                                        <i class="bx bxl-instagram"></i>
-                                        <a href="https://www.instagram.com/agrithec/" target="_blank" class="link-btn"></a>
-                                    </div>
-                                </li>
+            <div class="col-lg-3 col-sm-6">
+                <div class="single-footer-widget">
+                    <h3>Quick Links</h3>
+                    <ul class="quick-links">
+                        <li><a href="index.php">About Us</a></li>
+                        <li><a href="index.php">Our Services</a></li>
+                        <li><a href="index.php">Shop</a></li>
+                        <li><a href="index.php">Blog</a></li>
+                        <li><a href="index.php">Faq</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
 
-                                <li>
-                                    <div class="box">
-                                        <img src="assets/img/instagram/instagram3.jpg" alt="image">
-                                        <i class="bx bxl-instagram"></i>
-                                        <a href="https://www.instagram.com/agrithec/" target="_blank" class="link-btn"></a>
-                                    </div>
-                                </li>
-
-                                <li>
-                                    <div class="box">
-                                        <img src="assets/img/instagram/instagram4.jpg" alt="image">
-                                        <i class="bx bxl-instagram"></i>
-                                        <a href="https://www.instagram.com/agrithec/" target="_blank" class="link-btn"></a>
-                                    </div>
-                                </li>
-
-                                <li>
-                                    <div class="box">
-                                        <img src="assets/img/instagram/instagram5.jpg" alt="image">
-                                        <i class="bx bxl-instagram"></i>
-                                        <a href="https://www.instagram.com/agrithec/" target="_blank" class="link-btn"></a>
-                                    </div>
-                                </li>
-
-                                <li>
-                                    <div class="box">
-                                        <img src="assets/img/instagram/instagram6.jpg" alt="image">
-                                        <i class="bx bxl-instagram"></i>
-                                        <a href="https://www.instagram.com/agrithec/" target="_blank" class="link-btn"></a>
-                                    </div>
-                                </li>
-
-                                <li>
-                                    <div class="box">
-                                        <img src="assets/img/instagram/instagram7.jpg" alt="image">
-                                        <i class="bx bxl-instagram"></i>
-                                        <a href="https://www.instagram.com/agrithec/" target="_blank" class="link-btn"></a>
-                                    </div>
-                                </li>
-
-                                <li>
-                                    <div class="box">
-                                        <img src="assets/img/instagram/instagram8.jpg" alt="image">
-                                        <i class="bx bxl-instagram"></i>
-                                        <a href="https://www.instagram.com/agrithec/" target="_blank" class="link-btn"></a>
-                                    </div>
-                                </li>
-
-                                <li>
-                                    <div class="box">
-                                        <img src="assets/img/instagram/instagram1.jpg" alt="image">
-                                        <i class="bx bxl-instagram"></i>
-                                        <a href="https://www.instagram.com/agrithec/" target="_blank" class="link-btn"></a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="single-footer-widget">
-                            <h3>Quick Links</h3>
-    
-                            <ul class="quick-links">
-                                <li>
-                                    <a href="index.php">About Us</a>
-                                </li>
-                                <li>
-                                    <a href="index.php">Our Services</a>
-                                </li>
-                                <li>
-                                    <a href="index.php">Shop</a>
-                                </li>
-                                <li>
-                                    <a href="index.php">Blog</a>
-                                </li>
-                                <li>
-                                    <a href="index.php">Faq</a>
-                                </li>
-                                <li>
-                                    <a href="contact.php">Contact</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="single-footer-widget">
-                            <h3>Contact Us</h3>
-
-                            <ul class="footer-contact-info">
-                                <li>
-                                    <i class='bx bxs-phone'></i>
-                                    <span>Phone</span>
-                                    <a href="tel:407409202288">+213 660 62 30 72</a>
-                                </li>
-                                <li>
-                                    <i class='bx bx-envelope'></i>
-                                    <span>Email</span>
-                                    <a href="mailto:contact@agrithec.com">contact@agritech.com</a>
-                                </li>
-                                <li>
-                                    <i class='bx bx-map'></i>
-                                    <span>Address</span>
-                                    New Laghouat QRGG+3QC, Laghouat 
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="single-footer-widget">
+                    <h3>Contact Us</h3>
+                    <ul class="footer-contact-info">
+                        <li>
+                            <i class='bx bxs-phone'></i>
+                            <span>Phone</span>
+                            <a href="tel:<?= $data['phone'] ?>"><?= $data['phone'] ?></a>
+                        </li>
+                        <li>
+                            <i class='bx bx-envelope'></i>
+                            <span>Email</span>
+                            <a href="mailto:<?= $data['email'] ?>"><?= $data['email'] ?></a>
+                        </li>
+                        <li>
+                            <i class='bx bx-map'></i>
+                            <span>Address</span>
+                            <?= $newdata['location'] ?>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
+    </div>
+</div>
+
+
+
